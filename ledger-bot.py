642414301c74aa8435816c223be0c9445172a7c3
ManasBommakanti <manas.bommakanti@gmail.com>
2024-01-25 23:56:23 -0500

    updated arguments and pretty prints

--- ledger-bot.py
+++ ledger-bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.commands import Option
 import random
 
 import json
@@ -16,6 +17,7 @@ intents.message_content = True
 
 bot = discord.Bot()
 ledger = discord.SlashCommandGroup("ledger", "ledger command group")
+client = discord.Client()
 
 
 @bot.event
@@ -24,8 +26,17 @@ async def on_ready():
     print("------")
 
 
-@ledger.command()
-async def addplayer(ctx, name: str):
+@ledger.command(name="addplayer", description="Add your name to the Poker Ledger!")
+async def addplayer(ctx, member: discord.Member = None):
+    if member is None:
+        # If no member is specified, use the author's username
+        name = ctx.author.display_name
+    else:
+        # Use the specified member's username
+        name = member.display_name
+
+    print("USERNAME: ", name)
+
     # Load existing data from the ledger.json file
     try:
         with open("secrets/ledger.json", "r") as f:
@@ -33,10 +44,24 @@ async def addplayer(ctx, name: str):
     except FileNotFoundError:
         # If the file doesn't exist, initialize data as an empty dictionary
         data = {}
+        print("Cannot find secrets/ledger.json")
+    except Exception as e:
+        print(e)
+        embed = discord.Embed(
+            title="Error!",
+            description=f"Something is wrong internally :(",
+            color=discord.Colour.red(),
+        )
+        return await ctx.respond(embed=embed)
 
     # Check if the player with the given name already exists
     if name in data:
-        return await ctx.send("User already exists!")
+        embed = discord.Embed(
+            title="Error!",
+            description=f"Player with username **{name}** already exists.",
+            color=discord.Colour.red(),
+        )
+        return await ctx.respond(embed=embed)
 
     # Add the new player to the data
     data[name] = {
@@ -51,7 +76,13 @@ async def addplayer(ctx, name: str):
     with open("secrets/ledger.json", "w") as f:
         json.dump(data, f, indent=4)
 
-    await ctx.send(f"Player {name} added successfully!")
+    embed = discord.Embed(
+        title="Player Added!",
+        description=f"Welcome **{name}**!",
+        color=discord.Colour.green(),
+    )
+
+    await ctx.respond(embed=embed)
 
 
 @ledger.command()
@@ -64,20 +95,30 @@ async def showstats(ctx, name: str):
         # If the file doesn't exist, initialize data as an empty dictionary
         data = {}
 
-    # Check if the player with the given name already exists
+    # Check if the player with the given name does not exist
     if name not in data:
-        return await ctx.send(f"Player {name} does not exist!")
+        embed = discord.Embed(
+            title="Error!",
+            description=f"Player with username **{name}** is not in Ledger System.",
+            color=discord.Colour.red(),
+        )
+        return await ctx.respond(embed=embed)
 
     user_stats = data[name]
     stats_message = (
-        f"Stats for {name}:\n"
-        f"Bank: {user_stats['bank']}\n"
+        f"Bank: ${user_stats['bank']}\n"
         f"Hands Won: {user_stats['hands_won']}\n"
         f"Hands Lost: {user_stats['hands_lost']}\n"
         f"Folds: {user_stats['folds']}\n"
     )
 
-    await ctx.send(stats_message)
+    embed = discord.Embed(
+        title=f"Statistics for {name}",
+        description=stats_message,
+        colour=discord.Colour.blue(),
+    )
+
+    await ctx.respond(embed=embed)
 
 
 @ledger.command()
@@ -92,7 +133,7 @@ async def addwin(ctx, name: str):
 
     # Check if the player with the given name already exists
     if name not in data:
-        return await ctx.send(f"Player {name} does not exist!")
+        return await ctx.respond(f"Player {name} does not exist!")
 
     update_wins = data[name]["hands_won"] + 1
     data[name]["hands_won"] = update_wins
@@ -103,7 +144,7 @@ async def addwin(ctx, name: str):
 
     message = f"Added one win to Player {name}\n" f"Total Wins: {update_wins}\n"
 
-    await ctx.send(message)
+    await ctx.respond(message)
 
 
 @ledger.command()
@@ -118,11 +159,11 @@ async def removewin(ctx, name: str):
 
     # Check if the player with the given name already exists
     if name not in data:
-        return await ctx.send(f"Player {name} does not exist!")
+        return await ctx.respond(f"Player {name} does not exist!")
 
     # Check if hands_won = 0 because we cannot go negative
     if data[name]["hands_won"] <= 0:
-        return await ctx.send(
+        return await ctx.respond(
             (f"Player {name} did not win anything :(\n" f"Cannot remove from 0 wins")
         )
 
@@ -135,7 +176,7 @@ async def removewin(ctx, name: str):
 
     message = f"Removed one win to Player {name}\n" f"Total Wins: {update_wins}\n"
 
-    await ctx.send(message)
+    await ctx.respond(message)
 
 
 @ledger.command()
@@ -150,7 +191,7 @@ async def addloss(ctx, name: str):
 
     # Check if the player with the given name already exists
     if name not in data:
-        return await ctx.send(f"Player {name} does not exist!")
+        return await ctx.respond(f"Player {name} does not exist!")
 
     update_loss = data[name]["hands_lost"] + 1
     data[name]["hands_lost"] = update_loss
@@ -161,7 +202,7 @@ async def addloss(ctx, name: str):
 
     message = f"Added one win to Player {name}\n" f"Total Wins: {update_loss}\n"
 
-    await ctx.send(message)
+    await ctx.respond(message)
 
 
 @ledger.command()
@@ -176,15 +217,15 @@ async def removeloss(ctx, name: str):
 
     # Check if the player with the given name already exists
     if name not in data:
-        return await ctx.send(f"Player {name} does not exist!")
+        return await ctx.respond(f"Player {name} does not exist!")
 
     # Check if player has more than 0 losses in order to remove one
     if data[name]["hands_lost"] == 0:
-        return await ctx.send(f"Player {name} has 0 losses. Cannot remove loss!")
+        return await ctx.respond(f"Player {name} has 0 losses. Cannot remove loss!")
 
     # Check if hands_lost = 0 because we cannot go negative
     if data[name]["hands_lost"] <= 0:
-        return await ctx.send(
+        return await ctx.respond(
             (
                 f"Player {name} did not lose anything... yet\n"
                 f"Cannot remove from 0 losses"
@@ -200,7 +241,7 @@ async def removeloss(ctx, name: str):
 
     message = f"Removed one win to Player {name}\n" f"Total Wins: {update_loss}\n"
 
-    await ctx.send(message)
+    await ctx.respond(message)
 
 
 @ledger.command()
@@ -215,7 +256,7 @@ async def addfold(ctx, name: str):
 
     # Check if the player with the given name already exists
     if name not in data:
-        return await ctx.send(f"Player {name} does not exist!")
+        return await ctx.respond(f"Player {name} does not exist!")
 
     update_folds = data[name]["folds"] + 1
     data[name]["folds"] = update_folds
@@ -226,7 +267,7 @@ async def addfold(ctx, name: str):
 
     message = f"Added one win to Player {name}\n" f"Total Wins: {update_folds}\n"
 
-    await ctx.send(message)
+    await ctx.respond(message)
 
 
 @ledger.command()
@@ -241,11 +282,11 @@ async def removefold(ctx, name: str):
 
     # Check if the player with the given name already exists
     if name not in data:
-        return await ctx.send(f"Player {name} does not exist!")
+        return await ctx.respond(f"Player {name} does not exist!")
 
     # Check if folds = 0 because we cannot go negative
     if data[name]["folds"] <= 0:
-        return await ctx.send(
+        return await ctx.respond(
             (
                 f"Player {name} did not fold anytime (idk how)\n"
                 f"Cannot remove from 0 folds"
@@ -261,7 +302,7 @@ async def removefold(ctx, name: str):
 
     message = f"Removed one win to Player {name}\n" f"Total Wins: {update_fold}\n"
 
-    await ctx.send(message)
+    await ctx.respond(message)
 
 
 bot.add_application_command(ledger)
